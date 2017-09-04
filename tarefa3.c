561043babeff502faebd080c9388696521602ec2
daianagomes <daianagomes@id.uff.br>
2017-09-04 17:06:25 -0300

    Inserção da Matriz

---
+++ tarefa3.c
@@ -0,0 +1,43 @@
+main() {
+
+int i;
+float K;
+float x1, x2, x3, x4;
+
+	float t1[5], t2[5], t3[5], t4[5], ma[5];
+
+		
+		for(i=0; i<5; i++)
+			{
+				printf("Valor linha 1 posição[%d]: ", i);
+				scanf("%f", &t1[i]);
+			}
+
+			printf("\n");
+
+		for(i=0; i<5; i++)
+			{
+				printf("Valor linha 2 posição[%d]: ", i);
+				scanf("%f", &t2[i]);
+			}
+			
+			printf("\n");
+
+		for(i=0; i<5; i++)
+			{
+				printf("Valor linha 3 posição[%d]: ", i);
+				scanf("%f", &t3[i]);
+			}
+
+			printf("\n");
+
+		for(i=0; i<5; i++)
+			{
+				printf("Valor linha 4 posição[%d]: ", i);
+				scanf("%f", &t4[i]);
+			}
+	
+			printf("\n");
+
+
+					
